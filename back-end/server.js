d7695b06f63e4c844e2d5ebb271c035145e66b9d
Payton Viloria <paytonviloria@gmail.com>
2021-03-27 16:23:08 -0700

    Counties POST api endpoint works

--- back-end/server.js
+++ back-end/server.js
@@ -13,9 +13,33 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/todo', {
+mongoose.connect('mongodb://localhost:27017/vaccine-sites', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+});
+
+// Create a scheme for counties
+const countySchema = new mongoose.Schema({
+  name: String,
+  numVaccLocations: Number
+});
+
+// Create a model for counties
+const County = mongoose.model('County', countySchema);
+
+// Add a county
+app.post('/api/county', async (req, res) => {
+  const county = new County({
+    name: req.body.name,
+    numVaccLocations: 2
+  });
+  try {
+    await county.save();
+    res.send(county);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
