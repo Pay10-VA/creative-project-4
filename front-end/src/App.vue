0960311e0594420eecff4d27125b9433c262e07c
Payton Viloria <paytonviloria@gmail.com>
2021-03-27 14:38:55 -0700

    Home page front end mostly complete

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,32 +1,63 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="header">
+      <h1>Vaccine Finder</h1>
+      <div id="links">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
     </div>
     <router-view/>
+    <div id="footer">
+      <a>GitHub Repository</a>
+    </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  padding: 0;
+  margin: 0;
+}
+
+#header {
+  background-color: #3771D8;
+  color: #FFFFFF;
+  display: flex;
+  justify-content: space-around;
+  font-family: 'Quicksand', sans-serif;
+}
+
+#header > h1 {
+  font-size: 50px;
+  width: 50%;
+  margin-left: 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+#links {
+  width: 50%;
+  text-align: right;
+  margin-right: 20px;
+  margin-top: 30px;
+}
+
+#links a {
+  color: #FFFFFF;
+}
+
+#links a.router-link-exact-active {
+  color: yellow;
+}
+
+#footer {
+  background-color: #3771D8;
+  color: #FFFFFF;
+  font-family: 'Quicksand', sans-serif;
+  font-size: 20px;
+  height: 50px;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,18 +1,104 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong>Warning!</strong> This is not a real site. This is a school project. DO NOT use this site for medical or covid related information
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+
+    <div id="container">
+      <img id="state" src="../../public/Nevada_big_logo.png"/>
+    </div>
+
+    <h1 id="title">Find A Vaccination Center</h1>
+
+    <div id="form-container">
+      <h3>Select a County: </h3>
+      <form>
+
+        <select id=”lunch” name=”lunch”>
+          <option value=”carson”>Carson City</option>
+          <option value=”churchill”>Chuchill County</option>
+          <option value=”clark”>Clark County</option>
+          <option value=”douglas”>Douglas County</option>
+          <option value=”elko”>Elko County</option>
+          <option value=”humboldt”>Humbolt County</option>
+          <option value=”mineral”>Mineral County</option>
+          <option value=”nye”>Nye County</option>
+          <option value=”washoe”>Washoe County</option>
+        </select>
+      </form>
+
+
+
+
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style scoped>
+
+.home {
+  width: 85%;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  font-family: 'Quicksand', sans-serif;
+}
+
+#state {
+  height: 400px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#container {
+  width: 100%;
+  text-align: center;
+}
+
+#title {
+  font-size: 50px;
+}
+
+#form-container {
+  margin-top: 50px;
+}
+
+/* Desktop Styles */
+@media only screen and (min-width: 961px) {
+
+.home {
+  width: 100%;
+}
+
+#state {
+  height: 500px;
+}
+
+#title {
+  font-size: 60px;
+}
+
+#form-container {
+  margin-top: 20px;
+}
+
+
+}
+
+</style>
