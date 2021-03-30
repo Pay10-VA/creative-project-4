6bf91d533242eeba438c99928e0e4f3b7359e8a4
Payton Viloria <paytonviloria@gmail.com>
2021-03-30 14:23:22 -0700

    Port number changed to 3001 in server.js

--- back-end/server.js
+++ back-end/server.js
@@ -273,4 +273,4 @@ app.put('/api/appointment/:id', async (req, res) => {
 
 
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3000!'));
