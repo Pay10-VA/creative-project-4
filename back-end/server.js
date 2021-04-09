5d2377455ab755db7fadb721d02ca3bc540145b0
Payton Viloria <paytonviloria@gmail.com>
2021-04-08 19:27:42 -0700

    Changed port to 3005

--- back-end/server.js
+++ back-end/server.js
@@ -337,4 +337,4 @@ app.put('/api/appointment', async (req, res) => {
    }
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3005, () => console.log('Server listening on port 3005!'));
